ac7124ac8520dc8488e45f33af3761ff1e8cbe4e
mkinju1 <ghaxkdns105@naver.com>
2024-09-05 17:12:51 +0900

    0905

---
+++ demoproject3/src/main/java/edu/kh/demo/Demoproject3Application.java
@@ -0,0 +1,13 @@
+package edu.kh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproject3Application.class, args);
+	}
+
+}
---
+++ demoproject3/src/test/java/edu/kh/demo/Demoproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoproject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
