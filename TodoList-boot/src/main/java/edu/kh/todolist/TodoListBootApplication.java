a84b089cc902922b361605114da21b22beb1dd90
mkinju1 <ghaxkdns105@naver.com>
2024-09-06 17:37:18 +0900

    0906

--- TodoList-boot/src/main/java/edu/kh/todolist/TodoListBootApplication.java
+++ TodoList-boot/src/main/java/edu/kh/todolist/TodoListBootApplication.java
@@ -1,13 +1,13 @@
-package edu.kh.demo;
+package edu.kh.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demoproject3Application {
+public class TodoListBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demoproject3Application.class, args);
+		SpringApplication.run(TodoListBootApplication.class, args);
 	}
 
 }
